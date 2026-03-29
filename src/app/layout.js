909fda3c628ad81628c0e4758a450e5efc6128c4
丹羽康揮 <kouki.n11@icloud.com>
2026-03-29 13:19:58 +0900

    Google Search Console 所有権確認メタタグを追加

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="8ZWuL1GijqKfjMQHI9PYIsRNPV67sPpKsd2_Zeoyzok" />
       </head>
       <body style={{ backgroundColor: '#f2f2f0', color: '#111111' }}>
         <Suspense fallback={null}>
